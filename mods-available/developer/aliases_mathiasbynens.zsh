099f2b9e350916ba49324b2ca9821f8c5e6cad5a
Charlie Gorichanaz <charlie@gorichanaz.com>
2017-04-05 03:11:34 -0700

    Dev aliases move, dependencies

--- mods-available/developer/aliases_mathiasbynens.zsh
+++ mods-available/developer/aliases_mathiasbynens.zsh
@@ -60,7 +60,9 @@ alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup
 alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
 
 # IP addresses
-alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+# alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+# grc: ip: command not found
+alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en0"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
 
