2501c0a3b068839525503d334eda5caf78221539
Charlie Gorichanaz <charlie@gorichanaz.com>
2017-04-30 22:25:44 -0700

    Desktop changes

---
+++ mods-available/productivity_extra/install.sh
@@ -0,0 +1,19 @@
+
+#######################################
+#
+# Globals:
+#   MODS_ALL (string) Path to all modules that can be installed.
+# Arguments:
+#   lvl      (int)    Indentation level.
+# Returns:
+#   None
+#######################################
+install_productivity_extra() {
+  local modpath=$MODS_ALL/productivity_extra
+  local lvl=${1:-0} # 0 unless second param set
+
+  open '/usr/local/Caskroom/backblaze/latest/Backblaze Installer.app'
+
+}
+
+install_productivity_extra $lvl3
