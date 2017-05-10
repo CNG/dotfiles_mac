b016972eca87147602f20e8ff93f3a980d4e0aa5
Charlie Gorichanaz <charlie@gorichanaz.com>
2017-05-10 10:50:20 -0700

    Fix open_with functions

--- mods-available/macos/upgrade_defaults.sh
+++ mods-available/macos/upgrade_defaults.sh
@@ -53,7 +53,7 @@ open_with () {
   local domain=$1 && shift
   local arr=($@)
   for ext in "${arr[@]}"; do
-    duti -s $1 .$ext all
+    duti -s $domain .$ext all
   done
 }
 open_with org.videolan.vlc ASX DTS GXF M2V M3U M4V MPEG1 MPEG2 MTS MXF OGM PLS BUP A52 AAC B4S CUE DIVX DV FLV M1V M2TS MKV MOV MPEG4 OMA SPX TS VLC VOB XSPF DAT BIN IFO PART 3G2 AVI MPEG MPG FLAC M4A MP1 OGG WAV XM 3GP SRT WMV AC3 ASF MOD MP2 MP3 MP4 WMA MKA M4P
