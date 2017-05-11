d161e1176e159c2c1cb320e3c32382afc17b16a2
Charlie Gorichanaz <charlie@gorichanaz.com>
2017-05-10 20:30:40 -0700

    Install instagram-scraper

--- mods-available/productivity/upgrade.sh
+++ mods-available/productivity/upgrade.sh
@@ -3,3 +3,14 @@ loginitems -a 'Scroll Reverser'
 loginitems -a "Retina DisplayMenu" -p "/Applications/RDM.app"
 loginitems -a "Divvy"
 loginitems -a 'Clipboard History' -s false
+
+pip_install_upgrade () {
+  local pip upgrade
+  pip=$(type pip3 | tail -n 1 | cut -d' ' -f3) ||
+    pip=$(type pip | tail -n 1 | cut -d' ' -f3) ||
+    return 1
+  [[ ! $($pip list --format=legacy | grep -F "$1" > /dev/null) ]] || upgrade='--upgrade'
+  $pip install "$1" $upgrade
+}
+
+pip_install_upgrade 'instagram-scraper'
