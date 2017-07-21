bc4d49270b3158eb9445244e6bbb22bc814cd01a
Charlie Gorichanaz <charlie@gorichanaz.com>
2017-07-21 16:45:15 -0700

    Add Vim configuration

--- mods-available/developer/install.sh
+++ mods-available/developer/install.sh
@@ -18,6 +18,34 @@ install_developer() {
   # dtrace: failed to initialize dtrace: DTrace requires additional privileges
   sudo chmod u+s /usr/sbin/dtrace
 
+
+  # Need to think about best way to do Vim, and Sublime, since
+  # I don't like symlinking directories into dotfiles
+
+  # Install pathogen.vim but to bundle folder, requiring in .vimrc:
+  #   runtime bundle/vim-pathogen/autoload/pathogen.vim
+  mkdir -p ~/.vim/bundle &&
+    curl -LSso ~/.vim/bundle/pathogen.vim https://tpo.pe/pathogen.vim
+  cd ~/.vim/bundle
+  git clone git://github.com/tpope/vim-repeat.git
+  git clone git://github.com/tpope/vim-surround.git
+  git clone git://github.com/tpope/vim-speeddating.git
+  # this looks interesting, but I am doing too much new stuff already
+  # git clone git://github.com/tpope/vim-abolish.git
+  git clone git://github.com/tpope/vim-commentary.git
+  git clone https://github.com/scrooloose/nerdcommenter.git
+
+  # SnipMate
+  git clone https://github.com/tomtom/tlib_vim.git
+  git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+  git clone https://github.com/garbas/vim-snipmate.git
+  # Optional:
+  git clone https://github.com/honza/vim-snippets.git
+
+
+
+
+
 }
 
 install_developer $lvl3
