97cc50cc06283497238c119efbba9bab3f2eaa94
Charlie Gorichanaz <charlie@gorichanaz.com>
2017-07-21 23:30:10 -0700

    Add Indent Object plugin for Vim for Python

--- mods-available/developer/upgrade.sh
+++ mods-available/developer/upgrade.sh
@@ -47,6 +47,7 @@ upgrade_developer() {
   # git clone git://github.com/tpope/vim-abolish.git || true
   git clone git://github.com/tpope/vim-commentary.git || true
   git clone https://github.com/scrooloose/nerdcommenter.git || true
+  git clone git@github.com:michaeljsmith/vim-indent-object.git || true
 
   # SnipMate
   git clone https://github.com/tomtom/tlib_vim.git || true
