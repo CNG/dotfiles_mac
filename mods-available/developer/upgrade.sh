25b39e2b0cb7931712e857c99a0dba3e7aae97c5
Charlie Gorichanaz <charlie@gorichanaz.com>
2017-07-21 17:04:02 -0700

    Move Vim stuff to upgrade script, fix Pathogen

--- mods-available/developer/upgrade.sh
+++ mods-available/developer/upgrade.sh
@@ -28,6 +28,35 @@ upgrade_developer() {
   # defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder 1
   # # Don’t display the annoying prompt when quitting iTerm
   # defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+
+  # Need to think about best way to do Vim, and Sublime, since
+  # I don't like symlinking directories into dotfiles
+
+  # TODO: convert these to git pull if folder exists
+  
+  # Install pathogen.vim but to bundle folder, requiring in .vimrc:
+  #   runtime bundle/vim-pathogen/autoload/pathogen.vim
+  mkdir -p ~/.vim/bundle
+  # curl -LSso ~/.vim/bundle/pathogen.vim https://tpo.pe/pathogen.vim
+  cd ~/.vim/bundle
+  git clone git://github.com/tpope/vim-pathogen.git || true
+  git clone git://github.com/tpope/vim-repeat.git || true
+  git clone git://github.com/tpope/vim-surround.git || true
+  git clone git://github.com/tpope/vim-speeddating.git || true
+  # this looks interesting, but I am doing too much new stuff already
+  # git clone git://github.com/tpope/vim-abolish.git || true
+  git clone git://github.com/tpope/vim-commentary.git || true
+  git clone https://github.com/scrooloose/nerdcommenter.git || true
+
+  # SnipMate
+  git clone https://github.com/tomtom/tlib_vim.git || true
+  git clone https://github.com/MarcWeber/vim-addon-mw-utils.git || true
+  git clone https://github.com/garbas/vim-snipmate.git || true
+  # Optional:
+  git clone https://github.com/honza/vim-snippets.git || true
+
+
+
 }
 
 upgrade_developer $lvl3
